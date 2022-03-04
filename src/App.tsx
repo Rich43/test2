bb5010224173ee73bfef13295e7f06e38e615c57
Richard Ward <RichieS@GMail.com>
2022-03-04 14:53:35 +0000

    Add initial table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import TableView from "./view/TableView";
 
 function App() {
   return (
     <div className="App">
+      <TableView />
     </div>
   );
 }
