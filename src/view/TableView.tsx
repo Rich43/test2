cdfb0cf9ee1dcd7fdc01513f1cfc1d5f850bffd0
Richard Ward <RichieS@GMail.com>
2022-03-16 12:07:03 +0000

    Add conditional render

--- src/view/TableView.tsx
+++ src/view/TableView.tsx
@@ -4,6 +4,8 @@ import { FC } from "react";
 import { TableViewCells } from "./TableViewCells";
 import { CollapsibleTableViewRow } from "./CollapsableTableViewRow";
 import CollapsibleTableViewCell from "./CollapsibleTableViewCell";
+import { useAppSelector } from "../model/redux/hooks";
+import { selectRows } from "../model/redux/tableViewReducer";
 
 interface TableViewProps {
     headers: string[]
@@ -11,6 +13,8 @@ interface TableViewProps {
 }
 
 export const TableView: FC<TableViewProps> = ({ headers, rows }) => {
+    const tableViewSelector = useAppSelector(selectRows);
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -21,15 +25,19 @@ export const TableView: FC<TableViewProps> = ({ headers, rows }) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                        <>
-                            <CollapsibleTableViewCell row={row}/>
-                            <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                                <TableViewCells row={row}/>
-                            </TableRow>
-                            <CollapsibleTableViewRow row={row} open={true}/>
-                        </>
-                        )
+                    {rows.map((row) => {
+                            const matchingRow = tableViewSelector.find(reduxRow => reduxRow.id === row.id);
+                            const open = matchingRow ? matchingRow.open : false;
+                            return (
+                                <>
+                                    <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                                        <CollapsibleTableViewCell row={row}/>
+                                        <TableViewCells row={row}/>
+                                    </TableRow>
+                                    {open ? <CollapsibleTableViewRow row={row} open={true}/> : <></>}
+                                </>
+                            );
+                        }
                     )}
                 </TableBody>
             </Table>
