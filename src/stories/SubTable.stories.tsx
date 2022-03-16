fca8b644a962485c8fc4f10cb7eab8e3d88e931c
Richard Ward <RichieS@GMail.com>
2022-03-16 14:15:33 +0000

    Update gitignore, add SubTable to storybook and clean out junk.

--- src/stories/SubTable.stories.tsx
+++ src/stories/SubTable.stories.tsx
@@ -3,19 +3,19 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { SubTable } from "../view/SubTable";
 
 export default {
-  title: 'Tables/Sub Table',
-  component: SubTable,
-  argTypes: {
-    headers: { control: 'array' },
-    rows: { control: 'array' },
-  },
+    title: 'Tables/Sub Table',
+    component: SubTable,
+    argTypes: {
+        headers: { control: 'array' },
+        rows: { control: 'array' },
+    },
 } as ComponentMeta<typeof SubTable>;
 
 const Template: ComponentStory<typeof SubTable> = (args) => <SubTable {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  headers: ['Header 1', 'Header 2'],
-  rows: [{id: '1', name: 'Row 1'}, {id: '2', name: 'Row 2'}],
+    headers: ['Header 1', 'Header 2'],
+    rows: [{ id: '1', name: 'Row 1' }, { id: '2', name: 'Row 2' }],
 };
 
