ec80b9d8980f8b7642da323ea13c6fcd75e5e419
Richard Ward <RichieS@GMail.com>
2022-03-20 09:32:21 +0000

    Add ts node start to package.json and implement hello world page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
     res.send('GET request to homepage')
+});
+
+app.server = app.listen(3000, () => {
+    console.log('Listening on port 3000!');
 });
