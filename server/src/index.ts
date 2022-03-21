90c76ea6dbdbaf350f2e1c08615b4990dec35abf
Richard Ward <RichieS@GMail.com>
2022-03-21 09:18:55 +0000

    Finished backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,18 @@
+import * as fs from 'fs';
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req: any, res: any) => {
-    res.send('GET request to homepage')
+    res.writeHead(302, {
+        'Location': '/api'
+    });
+    res.end();
+});
+
+app.get('/api', (req: any, res: any) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(fs.readFileSync('api.json', 'utf8'));
 });
 
 app.server = app.listen(3000, () => {
