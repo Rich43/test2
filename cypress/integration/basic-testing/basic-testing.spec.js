23937c5f3eb932f539995b3390957291f0260a26
Richard Ward <RichieS@GMail.com>
2022-03-27 14:33:36 +0100

    Add fully functional test

--- cypress/integration/basic-testing/basic-testing.spec.js
+++ cypress/integration/basic-testing/basic-testing.spec.js
@@ -3,23 +3,26 @@
 const table = "1\t2013-01-01\t2013-01-31\tFAW\n" +
     "2\t2013-02-01\t2013-02-29\tPAED\n" +
     "3\t2013-03-04\t2013-03-05\tEFAW\n" +
-    "4\t2013-04-05\t2013-06-29\tEFAW".split("\n").map(line => line.split("\t"));
+    "4\t2013-04-05\t2013-06-29\tEFAW";
+const tableSplit = table.split("\n").map(line => line.split("\t"));
 
 describe('example to-do app', () => {
     beforeEach(() => {
         cy.visit('http://localhost')
     });
 
-    it('displays two todo items by default', () => {
+    it('displays table with correct data', () => {
         cy.get('tr').should('have.length', 5);
-        cy.get('tr').each(($el, index, $list) => {
-            if (index > 0) {
-                const tdElements = cy.wrap($el).find('td');
+        cy.get('tr').each(($rowEl, rowIndex, $list) => {
+            if (rowIndex > 0) {
+                const tdElements = cy.wrap($rowEl).find('td');
                 tdElements.should('have.length', 5);
-                tdElements.eq(1).should('contain', table[index][0]);
-                tdElements.eq(2).should('contain', table[index][1]);
-                tdElements.eq(3).should('contain', table[index][2]);
-                tdElements.eq(4).should('contain', table[index][3]);
+                tdElements.each(($colEl, columnIndex, $list) => {
+                    if (columnIndex > 0) {
+                        const colElements = cy.wrap($colEl);
+                        colElements.should('have.text', tableSplit[rowIndex - 1][columnIndex - 1]);
+                    }
+                });
             }
         });
     });
